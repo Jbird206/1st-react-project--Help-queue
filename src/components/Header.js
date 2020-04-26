229db8e5c09f6b838fcf8264eea96a6d3758581e
Priyaraj7 <suppichala@gmail.com>
2020-04-26 15:56:16 -0700

    add resuable file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-function Header() {
-  return <h1>Help Queue</h1>;
+function Header(props) {
+  const myStyledComponentStyles = {
+    backgroundColor: "#b3ecff",
+    fontFamily: "sans-serif",
+    padding: "50px",
+    margin: "50px",
+    marginLeft: 50,
+    marginRight: 50,
+    boxShadow: "5px 5px 15px",
+    borderRadius: 50,
+    textAlign: "center",
+    border: "1px solid cyan",
+    borderWidth: 2,
+    borderColor: "#8585ad",
+  };
+  return (
+    <div style={myStyledComponentStyles}>
+      <h1>Help Queue!</h1>
+      <p>Create a ticket so we can assist you.</p>
+    </div>
+  );
 }
 
 export default Header;
